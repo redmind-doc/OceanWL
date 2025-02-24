4ac6a598b0b0f3701f2467115ef825692722d69b
mbeuchee <malo.beuchee@gmail.com>
2025-02-24 14:56:10 +0100

    test concluant. changement pour créer le client. prochain test com avec RPI

--- client/com-BLE-client/com-BLE-client.ino
+++ client/com-BLE-client/com-BLE-client.ino
@@ -4,66 +4,79 @@
 #include <BLEAdvertisedDevice.h>
 
 // Définir les UUIDs pour le service et la caractéristique
-#define SERVICE_UUID "4fafc201-1fb5-459e-8fcc-c5c9331914b8"
-#define CHARACTERISTIC_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
+static BLEUUID serviceUUID("4fafc201-1fb5-459e-8fcc-c5c9331914b8");
+static BLEUUID charUUID("beb5483e-36e1-4688-b7f5-ea07361b26a8");
+static boolean doConnect = false;
+static boolean connected = false;
+static boolean doScan = false;
+static BLERemoteCharacteristic* pRemoteCharacteristic;
+static BLEAdvertisedDevice* myDevice;
 
-BLEClient* pClient;
-BLERemoteCharacteristic* pRemoteCharacteristic;
-bool connectToServer(BLEAdvertisedDevice* pServer);
+static void notifyCallback(
+  BLERemoteCharacteristic* pBLERemoteCharacteristic,
+  uint8_t* pData,
+  size_t length,
+  bool isNotify) {
+    Serial.print("Received Value: ");
+    Serial.println((char*)pData);
+}
 
 class MyClientCallback : public BLEClientCallbacks {
-    void onConnect(BLEClient* pClient) {
-      Serial.println("Connected to server");
-    }
+  void onConnect(BLEClient* pClient) {
+  }
 
-    void onDisconnect(BLEClient* pClient) {
-      Serial.println("Disconnected from server");
-    }
+  void onDisconnect(BLEClient* pClient) {
+    connected = false;
+  }
 };
 
 bool connectToServer(BLEAdvertisedDevice* pServer) {
   Serial.print("Connecting to ");
   Serial.println(pServer->getAddress().toString().c_str());
 
-  pClient = BLEDevice::createClient();
+  BLEClient* pClient = BLEDevice::createClient();
   pClient->setClientCallbacks(new MyClientCallback());
 
   pClient->connect(pServer);
 
-  BLERemoteService* pRemoteService = pClient->getService(SERVICE_UUID);
+  BLERemoteService* pRemoteService = pClient->getService(serviceUUID);
   if (pRemoteService == nullptr) {
     Serial.print("Failed to find service UUID: ");
-    Serial.println(SERVICE_UUID);
+    Serial.println(serviceUUID.toString().c_str());
     pClient->disconnect();
     return false;
   }
 
-  pRemoteCharacteristic = pRemoteService->getCharacteristic(CHARACTERISTIC_UUID);
+  pRemoteCharacteristic = pRemoteService->getCharacteristic(charUUID);
   if (pRemoteCharacteristic == nullptr) {
     Serial.print("Failed to find characteristic UUID: ");
-    Serial.println(CHARACTERISTIC_UUID);
+    Serial.println(charUUID.toString().c_str());
     pClient->disconnect();
     return false;
   }
 
+  pRemoteCharacteristic->registerForNotify(notifyCallback);
+  connected = true;
   return true;
 }
 
 class MyAdvertisedDeviceCallbacks: public BLEAdvertisedDeviceCallbacks {
-    void onResult(BLEAdvertisedDevice advertisedDevice) {
-      Serial.print("Advertised Device: ");
-      Serial.println(advertisedDevice.getAddress().toString().c_str());
+  void onResult(BLEAdvertisedDevice advertisedDevice) {
+    Serial.print("Advertised Device: ");
+    Serial.println(advertisedDevice.getAddress().toString().c_str());
 
-      if (advertisedDevice.haveServiceUUID() && advertisedDevice.isAdvertisingService(SERVICE_UUID)) {
-        BLEDevice::getScan()->stop();
-        connectToServer(&advertisedDevice);
-      }
+    if (advertisedDevice.haveServiceUUID() && advertisedDevice.isAdvertisingService(serviceUUID)) {
+      BLEDevice::getScan()->stop();
+      myDevice = new BLEAdvertisedDevice(advertisedDevice);
+      doConnect = true;
+      doScan = true;
     }
+  }
 };
 
 void setup() {
   Serial.begin(115200);
-  BLEDevice::init("");
+  BLEDevice::init("ESP32_Client");
   BLEScan* pBLEScan = BLEDevice::getScan();
   pBLEScan->setAdvertisedDeviceCallbacks(new MyAdvertisedDeviceCallbacks());
   pBLEScan->setInterval(1345);
@@ -72,7 +85,18 @@ void setup() {
 }
 
 void loop() {
-  if (pClient && pClient->isConnected()) {
+  if (doScan) {
+    if (doConnect) {
+      if (connectToServer(myDevice)) {
+        Serial.println("Connected to server");
+      } else {
+        Serial.println("Connection failed");
+      }
+      doConnect = false;
+    }
+  }
+
+  if (connected) {
     pRemoteCharacteristic->writeValue("Hello Raspberry Pi");
     delay(1000);
   }
